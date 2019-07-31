56d4f583809266c02b43e97d42f66bba99fbde22
Eitaro Fukamachi <e.arrows@gmail.com>
2019-07-31 17:24:56 +0900

    Add properties to Error\HttpRequest.

--- lib/Error/HttpRequest.php
+++ lib/Error/HttpRequest.php
@@ -5,8 +5,13 @@ use Exception;
 
 class HttpRequest extends Exception
 {
+    public $code;
+    public $response;
+
     public function __construct($code, $response)
     {
         parent::__construct("HTTP request failed (code=$code):\n  $response");
+        $this->code = $code;
+        $this->response = $response;
     }
 }
