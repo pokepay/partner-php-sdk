cf6d7328a490f3314a4a12f282cd3f48d28dc293
cxxxr <g23tlm@gmail.com>
2020-02-18 20:49:53 +0900

    fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,17 @@ use Faker\Factory;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    protected function newClient($iniFile = "/Users/cxxxr/.pokepay/test-config.ini")
+    protected function newClient($iniFile = null)
     {
+        if (!isset($iniFile)) {
+            $iniFile = (posix_getpwuid(posix_geteuid())['dir'] . "/.pokepay/test-config.ini");
+        }
         $config = parse_ini_file($iniFile);
         $this->testShopId = $config["shop_id"];
         $this->testCustomerId = $config["customer_id"];
         $this->testPrivateMoneyId = $config["private_money_id"];
+
+        $this->faker = Factory::create('ja_JP');
 
         return new PartnerAPI($iniFile);
     }
@@ -22,11 +27,18 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function randomPhoneNumber()
     {
-        return Factory::create('ja_JP')->phoneNumber;
+        $phoneNumber = '070';
+        for ($i = 0; $i < 2; $i++) {
+            $phoneNumber .= '-';
+            for ($j = 0; $j < 4; $j++) {
+                $phoneNumber .= mt_rand(0, 9);
+            }
+        }
+        return $phoneNumber;
     }
 
     protected function randomEmail()
     {
-        return Factory::create('ja_JP')->email;
+        return $this->faker->email;
     }
 }
